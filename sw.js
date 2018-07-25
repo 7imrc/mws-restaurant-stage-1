ebcc94d403c2649916984e6761705d94801bab1d
Tim Campbell <tim.campbell@btinternet.com>
2018-07-25 14:17:34 +0100

    chore:  Adding files to cache

--- sw.js
+++ sw.js
@@ -18,10 +18,17 @@ let cacheFiles = [
   './js/dbhelper.js',
   './js/main.js',
   './js/restaurant_info.js'
-]
+];
 
 self.addEventListener('install', (event) => {
   console.log('ServiceWorker installed');
+
+  event.waitUntil(
+    caches.open(cacheName).then( (cache) => {
+      console.log('sw caching files');
+      return cache.addAll(cacheFiles);
+    })
+  )
 })
 
 self.addEventListener('activate', (event) => {
